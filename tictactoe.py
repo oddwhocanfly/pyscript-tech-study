4355e501cb0cfee353e37a831deb5cbbbcf64f9c
Sasha Mokretsov <oddwhocanfly@gmail.com>
2023-02-03 02:24:00 +0400

    Display function

--- tictactoe.py
+++ tictactoe.py
@@ -1,1 +1,18 @@
-print('Hello, World!')
+from js import document #type: ignore
+
+def main():
+    display([
+        'T', 'O', 'E', 
+        'T', 'A', 'C', 
+        'T', 'I', 'C', 
+    ])
+
+def display(field):
+    display = document.getElementById("display")
+    display.innerHTML =\
+        field[6] + ' ' + field[7] + ' ' + field[8] + '<br>' +\
+        field[3] + ' ' + field[4] + ' ' + field[5] + '<br>' +\
+        field[0] + ' ' + field[1] + ' ' + field[2] + '<br>'
+
+if __name__ == '__main__':
+    main()
