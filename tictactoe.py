77f2f110619ebbcd3b11437affba650f1b00bb6a
Sasha Mokretsov <oddwhocanfly@gmail.com>
2023-02-04 01:27:42 +0400

    Key input, display() → draw()

--- tictactoe.py
+++ tictactoe.py
@@ -1,19 +1,40 @@
-from js import document #type: ignore
+import js #type: ignore
+import pyodide #type: ignore
+import asyncio
 
-def main():
-    display([
+async def main():
+    init()
+    draw([
         'T', 'O', 'E', 
         'T', 'A', 'C', 
         'T', 'I', 'C',
     ])
+    while True:
+        key = await get_key()
+        print(key)
 
-def display(board):
+def init():
+    on_key_down_proxy = pyodide.ffi.create_proxy(on_key_down)
+    js.document.addEventListener('keydown', on_key_down_proxy)
+
+def draw(board):
     assert len(board) == 9, '9 board elements expected'
-    display = document.getElementById("display")
+    display = js.document.getElementById('display')
     display.innerHTML =\
         board[6] + ' ' + board[7] + ' ' + board[8] + '<br>' +\
         board[3] + ' ' + board[4] + ' ' + board[5] + '<br>' +\
         board[0] + ' ' + board[1] + ' ' + board[2] + '<br>'
 
+async def get_key():
+    global get_key_key
+    get_key_key = None
+    while get_key_key == None:
+        await asyncio.sleep(0)
+    return get_key_key
+
+def on_key_down(event):
+    global get_key_key
+    get_key_key = event.code
+
 if __name__ == '__main__':
-    main()
+    asyncio.ensure_future(main())
